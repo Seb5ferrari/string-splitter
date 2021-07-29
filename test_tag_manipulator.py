18b02a8473d6427f83f0844eb10f97ba263bba69
Aditya Bhamidi <adityabhamidi@hotmail.com>
2021-07-29 12:50:53 +0200

    Added comma in between tokens test

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -60,3 +60,35 @@ def test_split_one_comma_start():
 
     # assert
     assert result == expResult
+
+
+def test_split_one_comma_end():
+    # arrange
+    stringToSplit = "java,"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+    print(result)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_comma_in_between_tokens():
+    # arrange
+    stringToSplit = "java, python"
+    regex = ","
+    expResult = ["java", "python"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+    print(result)
+
+    # assert
+    assert result == expResult
